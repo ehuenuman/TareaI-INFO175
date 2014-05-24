39a9a454f182d72e04d87cb251ea5bda5cd8c93d
ehuenuman <esteban.huenuman@hotmail.cl>
2014-05-24 01:59:20 -0400

    Métodos que manejan la base de datos

---
+++ controller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+
+def conectar():
+    con = sqlite3.connect('EmpresaDistribuidora.db')
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def obtener_productos():
+    con = conectar()
+    c = con.cursor()
+    query = "SELECT * FROM productos"
+    resultado = c.execute(query)
+    productos = resultado.fetchall()
+    con.close()
+    return productos
+
+
+def delete(rut):
+    exito = False
+    con = conectar()
+    c = con.cursor()
+    query = "DELETE FROM productos WHERE codigo = ?"
+    try:
+        resultado = c.execute(query, [rut])
+        con.commit()
+        exito = True
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    con.close()
+    return exito
+
+if __name__ == "__main__":
+
+    productos = obtener_productos()
+    for producto in productos:
+        print producto["nombre"]
