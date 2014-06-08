3c4f85555c5ea8fd8f38945a8b4ce0dcd37af1cb
ehuenuman <esteban.huenuman@hotmail.cl>
2014-06-08 14:24:13 -0400

    Visor para la ventana del formulario. Desde aquí se controlan las señales. Se añadio la señal para examinar imagenes del pc y añadirla a un producto especifico

--- view_nuevoproducto.py
+++ view_nuevoproducto.py
@@ -12,22 +12,20 @@ class Form (QtGui.QDialog):
     def __init__(self, code=None, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.codi = code
+        self.direccion_imagen = None
         if (self.codi is None):
             self.ui = Ui_nuevoProducto()
             self.ui.setupUi(self)
             self.ui.Btn_add.clicked.connect(self.add_valores)
             self.ui.Btn_cancel.clicked.connect(self.cancel)
-            self.dirI = self.ui.imagenButton.clicked.connect(
-                self.examinarImagen
-                )
+            self.ui.imagenButton.clicked.connect(self.examinarImagen)
         else:
             self.edit = Ui_nuevoProducto()
             self.edit.setupUi(self)
+            self.edit.setEdit(self)
             self.edit.Btn_add.clicked.connect(self.edit_valores)
             self.edit.Btn_cancel.clicked.connect(self.cancel)
-            self.dirI = self.edit.imagenButton.clicked.connect(
-                self.examinarImagen
-                )
+            self.edit.imagenButton.clicked.connect(self.examinarImagen)
 
     def cancel(self):
         self.reject()
@@ -35,7 +33,7 @@ class Form (QtGui.QDialog):
     def add_valores(self):
         cod = (self.ui.codigo.text())
         nom = (self.ui.nombre.text())
-        des = (self.ui.descripcion.text())
+        des = (self.ui.descripcion.toPlainText())
         col = (self.ui.color.text())
         pre = (self.ui.precio.text())
         mar = (self.ui.marca.text())
@@ -54,24 +52,41 @@ class Form (QtGui.QDialog):
                 id_marca = len(marcas) + 1
                 controller.ingresar_marca(id_marca, valores[5])
 
-            controller.ingresar_producto(valores)
-            self.almacenarImagen(self.dirI, cod)
-            self.reject()
+            exito = controller.ingresar_producto(valores)
+            if exito is False:
+                mensaje = u"Código ya existe.\nIntente con otro."
+                self.mensajeError(mensaje)
+            else:
+                if self.direccion_imagen is not None:
+                    self.almacenarImagen(self.direccion_imagen, cod)
+                self.reject()
         else:
             self.mensajeError(ingreso_valido)
 
     def llenarFormEditar(self, datos):
         self.edit.codigo.setText(datos[0])
         self.edit.nombre.setText(datos[1])
-        self.edit.descripcion.setText(datos[2])
+        self.edit.descripcion.setPlainText(datos[2])
         self.edit.color.setText(datos[3])
         self.edit.precio.setText(str(datos[4]))
         self.edit.marca.setText(datos[5])
+        ficheros = os.listdir('ImgProductos/')
+        fichero = datos[0] + ".jpg" in ficheros
+        if fichero is True:
+            direccion = 'ImgProductos/{0}'.format(datos[0] + ".jpg")
+            self.edit.setImageLabel(direccion)
+        else:
+            fichero = datos[0] + ".png" in ficheros
+            if fichero is True:
+                direccion = 'ImgProductos/{0}'.format(datos[0] + ".png")
+                self.edit.setImageLabel(direccion)
+            else:
+                self.edit.limpiarLabel()
 
     def edit_valores(self):
             cod = (self.edit.codigo.text())
             nom = (self.edit.nombre.text())
-            des = (self.edit.descripcion.text())
+            des = (self.edit.descripcion.toPlainText())
             col = (self.edit.color.text())
             pre = (self.edit.precio.text())
             mar = (self.edit.marca.text())
@@ -91,6 +106,8 @@ class Form (QtGui.QDialog):
                     controller.ingresar_marca(id_marca, valores[5])
 
                 controller.update(self.codi, valores)
+                if self.direccion_imagen is not None:
+                    self.almacenarImagen(self.direccion_imagen, cod)
                 self.reject()
             else:
                 self.mensajeError(ingreso_valido)
@@ -111,8 +128,10 @@ class Form (QtGui.QDialog):
                 return mensaje
             else:
                 if valido is False or valido2 is False or valido5 is False:
-                    mensaje = u"""Algunos campos fueron ingresados
-                              incorrectamente.\nCorrija e intente nuevamente."""
+                    mensaje = u"Algunos campos fueron ingresados"
+                    mensaje = mensaje + " incorrectamente.\n"
+                    mensaje = mensaje + "Recuerde que no se permiten simbolos."
+                    mensaje = mensaje + "\nCorrija e intente nuevamente."
                     return mensaje
                 else:
                     return True
@@ -128,25 +147,24 @@ class Form (QtGui.QDialog):
             "Abrir Imagenes", '', "Imagenes (*.png *.xpm *.jpg)")
             #";;All Files (*)")
 
-        direccion_imagen = nueva_imagen[0][0]
-
         try:
-            self.ui.setImageLabel(direccion_imagen)
+            self.direccion_imagen = nueva_imagen[0][0]
+        except:
+            self.direccion_imagen = None
+        try:
+            self.ui.setImageLabel(self.direccion_imagen)
         except:
             pass
         try:
-            self.edit.setImageLabel(direccion_imagen)
+            self.edit.setImageLabel(self.direccion_imagen)
         except:
             pass
-
-        return direccion_imagen
 
     def almacenarImagen(self, origen_imagen, nuevo_nombre):
         info = os.path.splitext(origen_imagen)
         extencion = info[1]
-        destino_imagen = "ImgProductos/{0}.{1}".format(nuevo_nombre, extencion)
+        destino_imagen = "ImgProductos/{0}{1}".format(nuevo_nombre, extencion)
         shutil.copy(origen_imagen, destino_imagen)
-        print "Copiada con exito"
 
 
 if __name__ == '__main__':
